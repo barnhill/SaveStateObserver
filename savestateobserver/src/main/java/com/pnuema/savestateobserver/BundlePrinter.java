8ecb064603b7b23a9950f772f540815bf69b1a51
Brad Barnhill <bradfordbarnhill@gmail.com>
2018-01-21 22:06:37 -0600

    fix lint warnings, fix travis build command

--- savestateobserver/src/main/java/com/pnuema/savestateobserver/BundlePrinter.java
+++ savestateobserver/src/main/java/com/pnuema/savestateobserver/BundlePrinter.java
@@ -9,12 +9,15 @@ import android.util.Log;
 import java.nio.CharBuffer;
 import java.util.Locale;
 
-public class BundlePrinter {
+public final class BundlePrinter {
     private static final String TAG = BundlePrinter.class.getSimpleName();
     private static final int SPACE_OFFSET = 4;
 
+    private BundlePrinter() {
+    }
+
     @SuppressWarnings("ConstantConditions")
-    public static void printBundleContents(@NonNull Class clazz, @NonNull Context context, @NonNull Bundle bundle) {
+    public static void printBundleContents(@NonNull final Class clazz, @NonNull final Context context, @NonNull final Bundle bundle) {
         if (context == null || bundle == null) {
             return;
         }
@@ -22,17 +25,17 @@ public class BundlePrinter {
         Log.d(TAG, context.getString(R.string.banner) + "\n" + getHeaderAsString(clazz, context, bundle) + "\n" + getContents(context, 1, bundle));
     }
 
-    private static String getHeaderAsString(@NonNull Class clazz, @NonNull Context context, @NonNull Bundle bundle) {
-        return context.getString(R.string.title, clazz.getSimpleName(), System.identityHashCode(bundle), bundle.size(), condense(context, getBundleTotalSize(bundle)));
+    private static String getHeaderAsString(@NonNull final Class clazz, @NonNull final Context context, @NonNull final Bundle bundle) {
+        return context.getResources().getQuantityString(R.plurals.title, bundle.size(), clazz.getSimpleName(), System.identityHashCode(bundle), bundle.size(), condense(context, getBundleTotalSize(bundle)));
     }
 
     @SuppressWarnings("ConstantConditions")
-    private static int getBundleTotalSize(@NonNull Bundle bundle) {
+    private static int getBundleTotalSize(@NonNull final Bundle bundle) {
         if (bundle == null) {
             return 0;
         }
 
-        Parcel parcel = Parcel.obtain();
+        final Parcel parcel = Parcel.obtain();
         try {
             parcel.writeBundle(bundle);
             return parcel.dataSize();
@@ -41,14 +44,16 @@ public class BundlePrinter {
         }
     }
 
-    private static String getContents(Context context, int layer, Bundle bundle) {
-        StringBuilder contents = new StringBuilder();
-        for (String key : bundle.keySet()) {
+    private static String getContents(final Context context, final int layer, final Bundle bundle) {
+        final StringBuilder contents = new StringBuilder();
+        for (final String key : bundle.keySet()) {
             if (bundle.get(key) instanceof Bundle) {
-                Bundle innerBundle = (Bundle) bundle.get(key);
-                contents.append(context.getString(R.string.bundle_entry_format, getPrefix(layer), System.identityHashCode(innerBundle), key, innerBundle.size(), condense(context, getBundleTotalSize(innerBundle))));
-                contents.append('\n');
-                contents.append(getContents(context, layer + 1, (Bundle) bundle.get(key)));
+                final Bundle innerBundle = (Bundle) bundle.get(key);
+                if (innerBundle != null) {
+                    contents.append(context.getResources().getQuantityString(R.plurals.bundle_entry_format, innerBundle.size(), getPrefix(layer), System.identityHashCode(innerBundle), key, innerBundle.size(), condense(context, getBundleTotalSize(innerBundle))));
+                    contents.append('\n');
+                    contents.append(getContents(context, layer + 1, (Bundle) bundle.get(key)));
+                }
             } else {
                 contents.append(context.getString(R.string.entry_format, getPrefix(layer), key, sizeOf(context, bundle, key)));
                 contents.append('\n');
@@ -58,16 +63,16 @@ public class BundlePrinter {
         return contents.toString();
     }
 
-    private static String sizeOf(Context context, Bundle bundle, String key) {
-        Parcel parcel = Parcel.obtain();
+    private static String sizeOf(final Context context, final Bundle bundle, final String key) {
+        final Parcel parcel = Parcel.obtain();
         parcel.writeValue(bundle.get(key));
-        int returnValue = parcel.dataSize();
+        final int returnValue = parcel.dataSize();
         parcel.recycle();
 
         return condense(context, returnValue);
     }
 
-    private static String condense(Context context, int bytes) {
+    private static String condense(final Context context, final int bytes) {
         if (bytes > 1000) {
             return context.getString(R.string.kilobyte_suffix, String.format(Locale.getDefault(), "%.2f", bytes / 1000f));
         } else {
@@ -75,7 +80,7 @@ public class BundlePrinter {
         }
     }
 
-    private static String getPrefix(int layer) {
+    private static String getPrefix(final int layer) {
         return CharBuffer.allocate(layer * SPACE_OFFSET).toString().replace('\0', ' ');
     }
 }
